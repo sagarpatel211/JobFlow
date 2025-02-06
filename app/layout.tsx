444d927c273ce484e4707a9b48f2ee63072c493b
Sagar Patel <spate275@ford.com>
2025-02-05 20:48:36 -0500

    Setting up navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
-import { DM_Sans } from 'next/font/google'
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Geist } from "next/font/google";
+import "./globals.css";
 
-const dmSans = DM_Sans({ subsets: ['latin'] })
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-export const metadata = {
-  title: 'JobFlow - Automate Your Job Search',
-  description: 'Track, apply, and land your dream job with ease using JobFlow.',
-}
+export const metadata: Metadata = {
+  title: "JobFlow",
+  description: "A tool to help automate your job search",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
-    <html lang="en" className="dark">
-      <body className={dmSans.className}>{children}</body>
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
